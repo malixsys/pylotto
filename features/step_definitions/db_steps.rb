ecd6191e75697b511d7a219eec3cb58a2dc46ee9
Martin Alix <github@malix.com>
2012-03-17 14:36:46 -0400

    trimmed directories

--- features/step_definitions/db_steps.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../support/my_net")
-
-When /^I save "([^"]*)"$/ do |input|
-  DB.save input
-end
-
-Then /^load should return "([^"]*)"$/ do |expected_output|
-  DB.load.should == expected_output
-end
